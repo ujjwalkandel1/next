1b905b600145d7860f6655adc754826fce4e2324
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-01-12 23:37:22 +0545

    first commit

---
+++ src/app/[Id]/page.jsx
@@ -0,0 +1,7 @@
+function Singlepage(){
+    return(
+        <h1>Hello this ia about Single page</h1>
+    )
+}
+
+export default Singlepage
---
+++ src/app/contact/page.js
@@ -0,0 +1,7 @@
+function Contact(){
+    return(
+        <h1>Hello this ia about page</h1>
+    )
+}
+
+export default Contact
