0edb4f3c3e33307a7c089e931583d63ffc2ff61c
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-01-13 23:29:48 +0545

    frontend part

---
+++ src/app/[Id]/edit/page.js
@@ -0,0 +1,10 @@
+import Form from "@/app/components/Form"
+
+
+function Editpage (){
+  return (
+    <Form />
+  )
+}
+
+export default Editpage
--- src/app/[Id]/page.jsx
+++
@@ -1,7 +0,0 @@
-function Singlepage(){
-    return(
-        <h1>Hello this ia about Single page</h1>
-    )
-}
-
-export default Singlepage
